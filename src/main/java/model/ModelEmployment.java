38e061854683cf3c578e70b441b0745200c80236
246133 <246133@KCSF0752.KCS-Learning.local>
2026-07-13 11:48:54 +0900

    a

--- src/main/java/model/ModelEmployment.java
+++ src/main/java/model/ModelEmployment.java
@@ -14,14 +14,14 @@ public class ModelEmployment {
     private String biko;              // 備考
 
     // 就職情報中間テーブル（複数）
-    private List<ShushokuJohoChukan> shushokuJohoChukanList;
+    private List<EmploymentChukan> shushokuJohoChukanList;
 
     public ModelEmployment() {}
 
     // コンストラクタ（必要に応じて拡張）
     public ModelEmployment(String shidoId, int gakusekiNo, String kaishaId,
     		LocalDateTime naiteiKakuteiBi, int naiteiKakutei, String biko,
-                           List<ShushokuJohoChukan> shushokuJohoChukanList) {
+                           List<EmploymentChukan> shushokuJohoChukanList) {
         this.shidoId = shidoId;
         this.gakusekiNo = gakusekiNo;
         this.kaishaId = kaishaId;
@@ -50,11 +50,11 @@ public class ModelEmployment {
     public String getBiko() { return biko; }
     public void setBiko(String biko) { this.biko = biko; }
 
-    public List<ShushokuJohoChukan> getShushokuJohoChukanList() {
+    public List<EmploymentChukan> getShushokuJohoChukanList() {
         return shushokuJohoChukanList;
     }
 
-    public void setShushokuJohoChukanList(List<ShushokuJohoChukan> shushokuJohoChukanList) {
+    public void setShushokuJohoChukanList(List<EmploymentChukan> shushokuJohoChukanList) {
         this.shushokuJohoChukanList = shushokuJohoChukanList;
     }
 }
